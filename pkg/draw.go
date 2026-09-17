a86d4d26a05d08d48123475c214eec27710143f8
Sean ONeill <seansaysmeh@gmail.com>
2026-09-17 16:56:33 +0100

    fix y orientation for wall texture

--- pkg/draw.go
+++ pkg/draw.go
@@ -70,43 +70,43 @@ func DrawRectWall(texture rl.Texture2D, position rl.Vector3, width, height, leng
 	rl.Color4ub(color.R, color.G, color.B, color.A)
 	// Front Face
 	rl.Normal3f(0.0, 0.0, 1.0) // Normal Pointing Towards Viewer
-	rl.TexCoord2f(0.0, 0.0)
-	rl.Vertex3f(x, y, z+length) // Bottom Left Of The Texture and Quad
-	rl.TexCoord2f(1.0, 0.0)
-	rl.Vertex3f(x+width, y, z+length) // Bottom Right Of The Texture and Quad
-	rl.TexCoord2f(1.0, 1.0)
-	rl.Vertex3f(x+width, y+height, z+length) // Top Right Of The Texture and Quad
 	rl.TexCoord2f(0.0, 1.0)
+	rl.Vertex3f(x, y, z+length) // Bottom Left Of The Texture and Quad
+	rl.TexCoord2f(1.0, 1.0)
+	rl.Vertex3f(x+width, y, z+length) // Bottom Right Of The Texture and Quad
+	rl.TexCoord2f(1.0, 0.0)
+	rl.Vertex3f(x+width, y+height, z+length) // Top Right Of The Texture and Quad
+	rl.TexCoord2f(0.0, 0.0)
 	rl.Vertex3f(x, y+height, z+length) // Top Left Of The Texture and Quad
 	// Back Face
 	rl.Normal3f(0.0, 0.0, -1.0) // Normal Pointing Away From Viewer
-	rl.TexCoord2f(1.0, 0.0)
-	rl.Vertex3f(x, y, z) // Bottom Right Of The Texture and Quad
 	rl.TexCoord2f(1.0, 1.0)
+	rl.Vertex3f(x, y, z) // Bottom Right Of The Texture and Quad
+	rl.TexCoord2f(1.0, 0.0)
 	rl.Vertex3f(x, y+height, z) // Top Right Of The Texture and Quad
-	rl.TexCoord2f(0.0, 1.0)
-	rl.Vertex3f(x+width, y+height, z) // Top Left Of The Texture and Quad
 	rl.TexCoord2f(0.0, 0.0)
+	rl.Vertex3f(x+width, y+height, z) // Top Left Of The Texture and Quad
+	rl.TexCoord2f(0.0, 1.0)
 	rl.Vertex3f(x+width, y, z) // Bottom Left Of The Texture and Quad
 	// Right face
 	rl.Normal3f(1.0, 0.0, 0.0) // Normal Pointing Right
-	rl.TexCoord2f(1.0, 0.0)
-	rl.Vertex3f(x+width, y, z) // Bottom Right Of The Texture and Quad
 	rl.TexCoord2f(1.0, 1.0)
+	rl.Vertex3f(x+width, y, z) // Bottom Right Of The Texture and Quad
+	rl.TexCoord2f(1.0, 0.0)
 	rl.Vertex3f(x+width, y+height, z) // Top Right Of The Texture and Quad
-	rl.TexCoord2f(0.0, 1.0)
-	rl.Vertex3f(x+width, y+height, z+length) // Top Left Of The Texture and Quad
 	rl.TexCoord2f(0.0, 0.0)
+	rl.Vertex3f(x+width, y+height, z+length) // Top Left Of The Texture and Quad
+	rl.TexCoord2f(0.0, 1.0)
 	rl.Vertex3f(x+width, y, z+length) // Bottom Left Of The Texture and Quad
 	// Left Face
 	rl.Normal3f(-1.0, 0.0, 0.0) // Normal Pointing Left
-	rl.TexCoord2f(0.0, 0.0)
-	rl.Vertex3f(x, y, z) // Bottom Left Of The Texture and Quad
-	rl.TexCoord2f(1.0, 0.0)
-	rl.Vertex3f(x, y, z+length) // Bottom Right Of The Texture and Quad
-	rl.TexCoord2f(1.0, 1.0)
-	rl.Vertex3f(x, y+height, z+length) // Top Right Of The Texture and Quad
 	rl.TexCoord2f(0.0, 1.0)
+	rl.Vertex3f(x, y, z) // Bottom Left Of The Texture and Quad
+	rl.TexCoord2f(1.0, 1.0)
+	rl.Vertex3f(x, y, z+length) // Bottom Right Of The Texture and Quad
+	rl.TexCoord2f(1.0, 0.0)
+	rl.Vertex3f(x, y+height, z+length) // Top Right Of The Texture and Quad
+	rl.TexCoord2f(0.0, 0.0)
 	rl.Vertex3f(x, y+height, z) // Top Left Of The Texture and Quad
 
 	rl.End()
