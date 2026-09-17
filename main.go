e02e5f3b54bfdad47f69d6209220f9003485f01e
Sean ONeill <seansaysmeh@gmail.com>
2026-09-17 16:52:39 +0100

    add wall

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"math"
 	"time"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 	"github.com/seanoneillcode/validation/pkg"
+)
+
+const (
+	MOUSE_LOOK_SENSITIVITY = 0.002
+	MOVE_SPEED             = 0.05
+)
+
+var (
+	FORWARD_VECTOR = rl.Vector3{X: 0, Y: 0, Z: 1}
+	UP_VECTOR      = rl.Vector3{X: 0, Y: 1, Z: 0}
 )
 
 func main() {
@@ -13,13 +24,15 @@ func main() {
 	rl.SetWindowState(rl.FlagVsyncHint)
 	rl.SetTargetFPS(60)
 
-	texture := rl.LoadTexture("clay.png")
+	textures := make([]rl.Texture2D, 10)
+	textures[0] = rl.LoadTexture("textures/clay.png")
+	textures[1] = rl.LoadTexture("textures/metal.png")
 
 	// setup
 	camera := createCamera()
 	s := state{
-		pos:     rl.NewVector3(0, 0, 0),
-		texture: texture,
+		pos:      rl.NewVector3(0, 1, 0),
+		textures: textures,
 	}
 
 	lastTime := time.Now()
@@ -29,13 +42,54 @@ func main() {
 		deltaTime := now.Sub(lastTime).Milliseconds()
 		lastTime = now
 		s.deltaTime = deltaTime
+		update(&camera, &s)
 
 		//draw
 		draw(camera, s)
 	}
-	rl.UnloadTexture(texture)
+	for _, t := range textures {
+
+		rl.UnloadTexture(t)
+
+	}
 
 	rl.CloseWindow()
+}
+
+func update(camera *rl.Camera3D, state *state) {
+	mouseDelta := rl.GetMouseDelta()
+	state.lookRotation -= mouseDelta.X * MOUSE_LOOK_SENSITIVITY
+
+	front := rl.NewVector3(float32(math.Sin(float64(state.lookRotation))), 0, float32(math.Cos(float64(state.lookRotation))))
+	right := rl.NewVector3(float32(math.Cos(float64(-state.lookRotation))), 0, float32(math.Sin(float64(-state.lookRotation))))
+
+	input := rl.Vector2{}
+	if rl.IsKeyDown(rl.KeyW) {
+		input.Y = 1
+	}
+	if rl.IsKeyDown(rl.KeyS) {
+		input.Y = -1
+	}
+	if rl.IsKeyDown(rl.KeyA) {
+		input.X = 1
+	}
+	if rl.IsKeyDown(rl.KeyD) {
+		input.X = -1
+	}
+	dir := rl.NewVector3(input.X*right.X+input.Y*front.X, 0, input.X*right.Z+input.Y*front.Z)
+
+	// speed := rl.Vector3DotProduct(hvel, dir);
+
+	state.velocity.X = dir.X * MOVE_SPEED
+	state.velocity.Z = dir.Z * MOVE_SPEED
+
+	state.pos.X = state.pos.X + (state.velocity.X)
+	state.pos.Z = state.pos.Z + (state.velocity.Z)
+
+	// update camera
+	yaw := rl.Vector3RotateByAxisAngle(FORWARD_VECTOR, UP_VECTOR, state.lookRotation)
+	camera.Target = rl.Vector3Add(state.pos, yaw)
+	camera.Position = state.pos
 }
 
 func draw(camera rl.Camera3D, state state) {
@@ -47,8 +101,16 @@ func draw(camera rl.Camera3D, state state) {
 	for i := range 10 {
 		for j := range 10 {
 			pos := rl.NewVector3(float32(i), 0, float32(j))
-			pkg.DrawRectFloor(state.texture, pos, 1, 1, rl.White)
+			pkg.DrawRectFloor(state.textures[0], pos, 1, 1, rl.White)
 		}
+	}
+	for j := range 5 {
+		pos := rl.NewVector3(float32(2), 0, float32(j))
+		pkg.DrawRectWall(state.textures[1], pos, 1, 1, 1, rl.White)
+	}
+	for j := range 5 {
+		pos := rl.NewVector3(float32(j), 0, float32(7))
+		pkg.DrawRectWall(state.textures[1], pos, 1, 1, 1, rl.White)
 	}
 	rl.DrawGrid(20, 1.0)
 
@@ -63,13 +125,16 @@ func createCamera() rl.Camera3D {
 	camera.Fovy = 45.0
 	camera.Projection = rl.CameraPerspective
 	camera.Position = rl.NewVector3(4, 4, 4)
+	rl.DisableCursor()
 	rl.UpdateCamera(&camera, rl.CameraCustom)
 	return camera
 }
 
 type state struct {
-	model     rl.Model
-	pos       rl.Vector3
-	texture   rl.Texture2D
-	deltaTime int64
+	model        rl.Model
+	pos          rl.Vector3
+	lookRotation float32
+	velocity     rl.Vector3
+	textures     []rl.Texture2D
+	deltaTime    int64
 }
--- pkg/draw.go
+++ pkg/draw.go
@@ -48,3 +48,69 @@ func DrawRectFloor(texture rl.Texture2D, position rl.Vector3, width, length floa
 
 	rl.SetTexture(defaultID)
 }
+
+func DrawRectWall(texture rl.Texture2D, position rl.Vector3, width, height, length float32, color rl.Color) {
+	x := position.X
+	y := position.Y
+	z := position.Z
+
+	// Set desired texture to be enabled while drawing following vertex data
+	defaultID := rl.GetTextureIdDefault()
+	rl.SetTexture(texture.ID)
+
+	// Vertex data transformation can be defined with the commented lines,
+	// but in this example we calculate the transformed vertex data directly when calling rlVertex3f()
+	// rl.PushMatrix()
+	// NOTE: Transformation is applied in inverse order (scale -> rotate -> translate)
+	//rl.Translatef(2.0, 0.0, 0.0)
+	//rl.Rotatef(45, 0, 1, )
+	//rl.Scalef(2.0, 2.0, 2.0)
+
+	rl.Begin(rl.Quads)
+	rl.Color4ub(color.R, color.G, color.B, color.A)
+	// Front Face
+	rl.Normal3f(0.0, 0.0, 1.0) // Normal Pointing Towards Viewer
+	rl.TexCoord2f(0.0, 0.0)
+	rl.Vertex3f(x, y, z+length) // Bottom Left Of The Texture and Quad
+	rl.TexCoord2f(1.0, 0.0)
+	rl.Vertex3f(x+width, y, z+length) // Bottom Right Of The Texture and Quad
+	rl.TexCoord2f(1.0, 1.0)
+	rl.Vertex3f(x+width, y+height, z+length) // Top Right Of The Texture and Quad
+	rl.TexCoord2f(0.0, 1.0)
+	rl.Vertex3f(x, y+height, z+length) // Top Left Of The Texture and Quad
+	// Back Face
+	rl.Normal3f(0.0, 0.0, -1.0) // Normal Pointing Away From Viewer
+	rl.TexCoord2f(1.0, 0.0)
+	rl.Vertex3f(x, y, z) // Bottom Right Of The Texture and Quad
+	rl.TexCoord2f(1.0, 1.0)
+	rl.Vertex3f(x, y+height, z) // Top Right Of The Texture and Quad
+	rl.TexCoord2f(0.0, 1.0)
+	rl.Vertex3f(x+width, y+height, z) // Top Left Of The Texture and Quad
+	rl.TexCoord2f(0.0, 0.0)
+	rl.Vertex3f(x+width, y, z) // Bottom Left Of The Texture and Quad
+	// Right face
+	rl.Normal3f(1.0, 0.0, 0.0) // Normal Pointing Right
+	rl.TexCoord2f(1.0, 0.0)
+	rl.Vertex3f(x+width, y, z) // Bottom Right Of The Texture and Quad
+	rl.TexCoord2f(1.0, 1.0)
+	rl.Vertex3f(x+width, y+height, z) // Top Right Of The Texture and Quad
+	rl.TexCoord2f(0.0, 1.0)
+	rl.Vertex3f(x+width, y+height, z+length) // Top Left Of The Texture and Quad
+	rl.TexCoord2f(0.0, 0.0)
+	rl.Vertex3f(x+width, y, z+length) // Bottom Left Of The Texture and Quad
+	// Left Face
+	rl.Normal3f(-1.0, 0.0, 0.0) // Normal Pointing Left
+	rl.TexCoord2f(0.0, 0.0)
+	rl.Vertex3f(x, y, z) // Bottom Left Of The Texture and Quad
+	rl.TexCoord2f(1.0, 0.0)
+	rl.Vertex3f(x, y, z+length) // Bottom Right Of The Texture and Quad
+	rl.TexCoord2f(1.0, 1.0)
+	rl.Vertex3f(x, y+height, z+length) // Top Right Of The Texture and Quad
+	rl.TexCoord2f(0.0, 1.0)
+	rl.Vertex3f(x, y+height, z) // Top Left Of The Texture and Quad
+
+	rl.End()
+	//rl.PopMatrix()
+
+	rl.SetTexture(defaultID)
+}
